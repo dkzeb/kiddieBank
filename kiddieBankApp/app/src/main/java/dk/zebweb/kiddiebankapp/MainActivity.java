bcf0f0300f7b0f9158c852353d190e28a4c0bb32
zeb <sthazeb@gmail.com>
2016-03-12 17:49:44 +0100

    Main og stuff! :)

--- kiddieBankApp/app/src/main/java/dk/zebweb/kiddiebankapp/MainActivity.java
+++ kiddieBankApp/app/src/main/java/dk/zebweb/kiddiebankapp/MainActivity.java
@@ -1,7 +1,10 @@
 package dk.zebweb.kiddiebankapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button barnBtn = (Button) findViewById(R.id.barnButton);
+        barnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), youGotMoney.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- kiddieBankApp/app/src/main/java/dk/zebweb/kiddiebankapp/youGotMoney.java
+++ kiddieBankApp/app/src/main/java/dk/zebweb/kiddiebankapp/youGotMoney.java
@@ -1,7 +1,11 @@
 package dk.zebweb.kiddiebankapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class youGotMoney extends AppCompatActivity {
@@ -16,5 +20,15 @@ public class youGotMoney extends AppCompatActivity {
             moneyImage.setImageDrawable("DK1000");
         }
 */
+
+        Button nextBtn = (Button) findViewById(R.id.nxtBtn);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), WishList.class);
+                i.putExtra("amount", 1000);
+                startActivity(i);
+            }
+        });
     }
 }
